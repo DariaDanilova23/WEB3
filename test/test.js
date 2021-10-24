27f681989cbabaf59242fc814b6dd2efe752a74f
DariaDanilova23 <84722879+DariaDanilova23@users.noreply.github.com>
2021-10-24 13:10:17 +0300

    Update test.js

--- test/test.js
+++ test/test.js
@@ -2,10 +2,8 @@ localStorage.setItem("Тест","+");
 
 setCookie('name','Тест',12);
 
-function setCookie(name_s, value_s, days){
+function setCookie(name, value, days){
 	var date = new Date;
-	var name=name_s;
-	var value=value_s;
 	date.setDate(date.getDate()+days);
 	value=encodeURIComponent(value);
 	document.cookie=name+"="+value+";path=/; expires="+date.toUTCString();
