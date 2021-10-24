a9e253cac819c3ff49f82844f0e5250d3b0b0b5b
DariaDanilova23 <84722879+DariaDanilova23@users.noreply.github.com>
2021-10-24 14:19:32 +0300

    Update about.js

--- about/about.js
+++ about/about.js
@@ -1,1 +1,10 @@
 localStorage.setItem("Обо мне","+");
+
+setCookie('Обо мне','я',12);
+
+function setCookie(name, value, days){
+	var date = new Date;
+	date.setDate(date.getDate()+days);
+	value=encodeURIComponent(value);
+	document.cookie=name+"="+value+";path=/; expires="+date.toUTCString();
+};
