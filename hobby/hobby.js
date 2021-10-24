6bef4bd7b1739d14d9b408e1b5802fba61b6a161
DariaDanilova23 <84722879+DariaDanilova23@users.noreply.github.com>
2021-10-24 14:16:52 +0300

    Update hobby.js

--- hobby/hobby.js
+++ hobby/hobby.js
@@ -6,7 +6,6 @@ function setCookie(name, value, days){
 	var date = new Date;
 	date.setDate(date.getDate()+days);
 	value=encodeURIComponent(value);
-	name=encodeURIComponent(name);
 	
 	document.cookie=name+"="+value+";path=/; expires="+date.toUTCString();
 };
