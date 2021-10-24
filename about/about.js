ff48bec6beff8db381f50be9df6fd4d5b84b64c4
DariaDanilova23 <84722879+DariaDanilova23@users.noreply.github.com>
2021-10-24 17:34:35 +0300

    Update about.js

--- about/about.js
+++ about/about.js
@@ -6,5 +6,5 @@ function setCookie(name, value, days){
 	var date = new Date;
 	date.setDate(date.getDate()+days);
 	value=encodeURIComponent(value);
-	document.cookie=name+value+";path=/; expires="+date.toUTCString();
+	document.cookie=name+"="+value+";path=/; expires="+date.toUTCString();
 };
