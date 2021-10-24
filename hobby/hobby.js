079de4625259673be534109d04dfcf7a3b789025
DariaDanilova23 <84722879+DariaDanilova23@users.noreply.github.com>
2021-10-24 14:10:57 +0300

    Update hobby.js

--- hobby/hobby.js
+++ hobby/hobby.js
@@ -1,6 +1,6 @@
  localStorage.setItem("Хобби","+");
 
-setCookie('help','Мои интересы',12);
+setCookie('Мои интересы','Мои интересы',12);
 
 function setCookie(name, value, days){
 	var date = new Date;
