ca8f4270bc666f9e844f427a8054c6bdc586445d
DariaDanilova23 <84722879+DariaDanilova23@users.noreply.github.com>
2021-10-24 14:23:15 +0300

    Update contact.js

--- contact/contact.js
+++ contact/contact.js
@@ -5,6 +5,7 @@ setCookie('Контакт','Конт',12);
 function setCookie(name, value, days){
 	var date = new Date;
 	date.setDate(date.getDate()+days);
+	value=encodeURIComponent(value);
 	document.cookie=name+"="+value+";path=/; expires="+date.toUTCString();
 };
 function checkParams(){
